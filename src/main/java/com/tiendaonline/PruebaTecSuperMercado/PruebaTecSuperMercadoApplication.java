172f65824171eb333344e2a158adc6fb5389dc42
omar.grimsa <omar.grimsa@gmail.com>
2025-11-20 12:43:32 -0600

    Initial commit

---
+++ src/main/java/com/tiendaonline/PruebaTecSuperMercado/PruebaTecSuperMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaonline.PruebaTecSuperMercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecSuperMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecSuperMercadoApplication.class, args);
+	}
+
+}
